822564e3fe1a68533336f01c12c84946acdfc58e
Ale San <alesm91@hotmail.com>
2024-10-07 18:02:25 +0200

    deleted unused code

--- src/test/java/com/bolsadeideas/springboot/app/SpringBootChallengeApplicationTests.java
+++ src/test/java/com/bolsadeideas/springboot/app/SpringBootChallengeApplicationTests.java
@@ -2,17 +2,8 @@ package com.bolsadeideas.springboot.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource("classpath:application-test.properties")
-/*@TestPropertySource(properties = {
-		"spring.jpa.hibernate.ddl-auto=none", // Disabled the auto-configuration of JPA
-		"spring.datasource.url=jdbc:h2:mem:testdb", // Use H2 in memory if necessary
-		"spring.datasource.driver-class-name=org.h2.Driver",
-		"spring.datasource.username=sa",
-		"spring.datasource.password=password"
-})*/
 class SpringBootChallengeApplicationTests {
 
 	@Test
